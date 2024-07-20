926b1f6be53aacbfa4606f08e93a8a7c5ad16d28
Katharine Chui <kwchuiaa@connect.ust.hk>
2024-07-20 02:09:42 +0200

    fix building

--- config.sh
+++ config.sh
@@ -57,7 +57,7 @@ fi
 
 # install input methods and some fonts for fallback
 set +e
-zypper -n --gpg-auto-import-keys install 'google-noto-*' 'noto-*-fonts' 'ibus-*'
+zypper -n --gpg-auto-import-keys install 'google-noto-*' 'ibus-*'
 status_code=$?
 if [ "$status_code" != "107" ] && [ "$status_code" != "0" ]
 then
@@ -88,13 +88,12 @@ then
 	done
 fi
 
-
 # disable root
 passwd -l root
 # steamos update dummys sudoer file is 99
 echo '%wheel  ALL=(ALL)       ALL' > /etc/sudoers.d/98_deck_pw_sudo
-sed -i'' '/^ALL   ALL=(ALL) ALL/d' /etc/sudoers
-sed -i'' '/^Defaults targetpw/d' /etc/sudoers
+sed -i'' '/^ALL   ALL=(ALL) ALL/d' /usr/etc/sudoers
+sed -i'' '/^Defaults targetpw/d' /usr/etc/sudoers
 
 # configure snapper as https://build.opensuse.org/package/show/openSUSE:Factory/openSUSE-MicroOS would
 cp /usr/share/snapper/config-templates/default /etc/snapper/configs/root
@@ -125,33 +124,29 @@ chown deck:deck /home/deck/.profile
 # cups access from group wheel
 sed -i'' 's/^SystemGroup root/SystemGroup wheel/' /etc/cups/cups-files.conf
 
-# install kernel-default
+# install kernel-default and lock it
 # on modern mainline kernels, emmc would corrupt without amd_iommu=off it seems
 # actually no the emmc is just too finicky on mainline, would corrupt even with all power saving features disabled
-#zypper -n --gpg-auto-import-keys install kernel-default
-
-# libblkid.so symlink missing work-around
-#kernel-install add *-default /lib/modules/*-default/vmlinuz
+zypper -n --gpg-auto-import-keys install kernel-default
+zypper -n addlock kernel-default
+rm /boot/vmlinuz*
+rm /boot/initrd*
 
 # install steamos kernels
 mkdir steamos_kernel
 cd steamos_kernel
 
-if false
-then
-	wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/linux-neptune-5.13.0.valve36-1-x86_64.pkg.tar.zst -O - | zstd -d -T0 | tar -xv
-	mv usr/lib/modules/5.13.0-valve36-1-neptune /usr/lib/modules/5.13.0-valve36-1-neptune
-	cp /usr/lib/modules/5.13.0-valve36-1-neptune/vmlinuz /boot/vmlinuz-5.13.0-valve36-1-neptune
-	kernel-install add 5.13.0-valve36-1-neptune /usr/lib/modules/5.13.0-valve36-1-neptune/vmlinuz
-fi
-
 # latest known good kernel with the emmc, and dpms on par with 5.13
-wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-3.5/os/x86_64/linux-neptune-61-6.1.52.valve3-1-x86_64.pkg.tar.zst -O - | zstd -d -T0 | tar -xv
-mv usr/lib/modules/6.1.52-valve3-1-neptune-61 /usr/lib/modules/6.1.52-valve3-1-neptune-61
-cp /usr/lib/modules/6.1.52-valve3-1-neptune-61/vmlinuz /boot/vmlinuz-6.1.52-valve3-1-neptune-61
-kernel-install add 6.1.52-valve3-1-neptune-61 /usr/lib/modules/6.1.52-valve3-1-neptune-61/vmlinuz
+#wget "https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-3.6/os/x86_64/linux-neptune-65-6.5.0.valve5-1-x86_64.pkg.tar.zst" -O - | zstd -d -T0 | tar -xv
+wget "https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-3.6/os/x86_64/linux-neptune-61-6.1.52.valve9-1-x86_64.pkg.tar.zst" -O - | zstd -d -T0 | tar -xv
+VERSION=6.1.52-valve9-1-neptune-61
+mv usr/lib/modules/$VERSION /usr/lib/modules/$VERSION
+cp /usr/lib/modules/$VERSION/vmlinuz /boot/vmlinuz-$VERSION
+kernel-install add $VERSION /usr/lib/modules/$VERSION/vmlinuz
 
 cd /
 rm -rf steamos_kernel
 
 zypper -n clean -a
+
+
